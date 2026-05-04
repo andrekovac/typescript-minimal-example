8d843b05a194cb4e691b33cfd53b8be751110a55
André Kovac <me@andrekovac.com>
2026-05-04 15:56:34 +0800

    fix: add kind property to Person type and initialize person1

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
+enum PersonKind {
+  Student = "student",
+  Teacher = "teacher",
+}
+
 type Person = {
   name: string; // required
   age?: number; // optional
+  kind: PersonKind;
 };
 
 const person1: Person = {
   name: "André",
   age: 39,
+  kind: PersonKind.Teacher,
 };
 
 const getPersonDetails = (person: Person): string => {
